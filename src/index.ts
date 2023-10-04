202afbf89e016e16cd3050b8319c33efe2df4b42
ThomasLaforge <thomas.laforge.38@gmail.com>
2023-10-04 13:06:13 +0200

    server init

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,50 @@
-function getAllDiceCombinations(dices: any[]) {
-  const toCombine = new Array(dices.length).fill("").map((_, i) => i)
-  let combinations: any[] = []
-  let temp: any[] = []
-  let slent = Math.pow(2, toCombine.length) - 1
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { Client } from 'pg'
 
-  for (let i = 0; i < slent; i++) {
-    temp = []
-    for (var j = 0; j < toCombine.length; j++) {
-      if (i & Math.pow(2, j)) {
-        temp.push(toCombine[j])
-      }
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/users/auth', (req, res) => {
+    if(req.body.login === 'admin' && req.body.password === 'toto') {
+        res.send({ status: "success" });
     }
-
-    if (temp.length > 0) {
-      combinations.push(temp)
+    else {
+        res.send({ status: "error" });
     }
-  }
+});
 
-  return combinations.map((combi) => {
-    const completion = toCombine.filter((index) => combi.indexOf(index) === -1)
-    const diceCombi = combi.map((e) => dices[e])
-    const diceCompletion = completion.map((e) => dices[e])
-    return [diceCombi, diceCompletion]
-  })
-}
+app.get('/books', (req, res) => {
+    res.json({ books: [
+        {
+            id: 1,
+            title: 'Harry Potter',
+            description: "jeune à lunettes"
+        },
+        {
+            id: 2,
+            title: 'Harry Potter 2',
+            description: "jeune à lunettes mais c'est le 2"
+        },
+        {
+            id: 3,
+            title: 'Mary Poppins',
+            description: "une nounou magique"
+        },
+        {
+            id: 4,
+            title: 'Star Wars',
+            description: "qui est ton père ?"
+        },
+    ]});
+});
 
-console.log('test fonction', getAllDiceCombinations(["vert", "rouge", "jaune"]))
+app.get('/users/admin/books', (req, res) => {
+    res.json({ user: { login: "admin"}, books: [1, 4] });
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
